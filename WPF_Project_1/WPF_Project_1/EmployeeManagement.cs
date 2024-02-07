eb5715e5cd066088be17ead7409bc59f767b50ac
Max <Zholdashev.Maks@yandex.ru>
2024-02-07 13:53:11 +0300

    Add EmployeeManagement

---
+++ WPF_Project_1/WPF_Project_1/EmployeeManagement.cs
@@ -0,0 +1,17 @@
+public partial class EmployeeManagement: Window
+{
+    public EmployeeManagemen()
+    {
+        InitializeComponent();
+    }
+
+    private void DeleteEmployee_Click(object sender, RoutedEventArgs e)
+    {
+        //  од дл€ удалени€ выбранного сотрудника
+    }
+
+    private void AddEmployee_Click(object sender, RoutedEventArgs e)
+    {
+        //  од дл€ добавлени€ нового сотрудника
+    }
+}
