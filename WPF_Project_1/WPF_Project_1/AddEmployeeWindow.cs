4edd6ed4c052da3540fca5098296ca3cea0856a9
Max <Zholdashev.Maks@yandex.ru>
2024-02-08 12:38:09 +0300

    HotFix 2

--- WPF_Project_1/WPF_Project_1/AddEmployeeWindow.cs
+++ WPF_Project_1/WPF_Project_1/AddEmployeeWindow.cs
@@ -1,7 +1,24 @@
-public partial class AddEmployeeWindow : Window
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Project_1
 {
-    public AddEmployeeWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AddEmployeeWindow : Window
     {
-        InitializeComponent();
+        public AddEmployeeWindow()
+        {
+            InitializeComponent();
+        }
     }
 }
--- WPF_Project_1/WPF_Project_1/EmployeeManagement.cs
+++ WPF_Project_1/WPF_Project_1/EmployeeManagement.cs
@@ -1,17 +1,32 @@
-public partial class EmployeeManagement: Window
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Project_1
 {
-    public EmployeeManagemen()
-    {
-        InitializeComponent();
-    }
 
-    private void DeleteEmployee_Click(object sender, RoutedEventArgs e)
+    public partial class EmployeeManagement : Window
     {
-        //  од дл€ удалени€ выбранного сотрудника
-    }
+        public EmployeeManagement()
+        {
 
-    private void AddEmployee_Click(object sender, RoutedEventArgs e)
-    {
-        //  од дл€ добавлени€ нового сотрудника
+        }
+
+        private void DeleteEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            //  од дл€ удалени€ выбранного сотрудника
+        }
+
+        private void AddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            //  од дл€ добавлени€ нового сотрудника
+        }
     }
 }
